2300ce3d1a3ff375ae59ca3365698a7421623065
abhisek11 <abhisekrock94@gmail.com>
2025-12-01 01:56:12 +0530

    refactor get_all_blog function and add get_blog_commentid endpoint

--- main.py
+++ main.py
@@ -9,14 +9,28 @@ app = FastAPI()
 def index():
     return {'message': 'Hello World'}
 
+
 @app.get('/blogs/all')
 def get_all_blog(page=1, page_size: Optional[int]=None):
-    return {"message": f" blogs {page} page and {page_size} page size"}
+    return {"message": f" All {page_size} blogs on page {page}"}
+
+
+@app.get('/blog/{id}/comment/{comment_id}')
+def get_blog_commentid(id: int, comment_id: int, valid: bool, username: Optional[str]=None):
+    return {
+        "message": {
+        'blog_id': id,
+        'comment_id': comment_id,
+        'valid': valid,
+        'username': username
+    }}
+
 
 class BlogType(str, Enum):
     short = 'short'
     story = 'story'
     howto = 'howto'
+
 
 @app.get('/blog/type/{type}')
 def get_blog_type(type: BlogType):
