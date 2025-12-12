fc51a4181482c1afe6f8ffd256cd4cd50d514a3c
abhisek11 <abhisekrock94@gmail.com>
2025-12-13 03:23:55 +0530

    refactor: update blog post and comment models for improved structure and type handling

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, Response, Query, Body, Path
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 
 router = APIRouter(
@@ -8,11 +8,18 @@ router = APIRouter(
     tags=['blog']
 )
 
+class Image(BaseModel):
+    url: str
+    alies: str
+
 class BlogModel(BaseModel):
     title: str
     content: str
     published: Optional[bool]
     no_of_comments: int
+    tags: List[str] = []
+    metadata: Dict[str, str] = {'key1': 'val1'}
+    image: Optional[Image] = None
 
 @router.post('/new/{id}')
 def create_post(request: Request, blog: BlogModel, id: int, version: int=1):
@@ -24,24 +31,26 @@ def create_post(request: Request, blog: BlogModel, id: int, version: int=1):
         "version": version
         }
 
-@router.post("/new/{id}/comment")
+@router.post("/new/{id}/comment/{comment_id}")
 def create_comment(blog: BlogModel, id: int,
                 comment_title: str = Query(
                     title="Id of the commnet",
                     description= "Some description for comment_Title",
                     alias="commentTitle",
                 ),
-                content=Body(...,
+                content: str = Body(...,
                             min_length=10,
                             max_length=12,
                             regex='^[a-z\s]*$'
                 ),
-                v: Optional[List[str]] = Query(['1.0', '1.1', '1.2'])
+                v: Optional[List[str]] = Query(['1.0', '1.1', '1.2']),
+                comment_id: int = Path(gt=5, le=45)
             ):
     return {
         "id": id,
         "blog": blog,
         "comment_title": comment_title, 
         "content": content,
-        "v": v
+        "version": v,
+        "comment_id": comment_id
         }
